54efad591109b2fb9436c243fe644ccb16e6701f
sohailnagra <31482995+sohailnagra@users.noreply.github.com>
2021-04-18 23:34:29 +0500

    mul function

--- tb/tb_gcm_ghash.v
+++ tb/tb_gcm_ghash.v
@@ -1,63 +1,65 @@
 module tb_gcm_ghash ();
 
 /**********************************************************************
-* Inputs as registers 
+* Inputs as registers
 **********************************************************************/
-reg         clk     ; 
-reg         reset_n ; 
-reg         init    ; 
-reg         next    ; 
-reg [127:0] h0      ; 
-reg [127:0] x       ; 
+  reg         clk    ;
+  reg         reset_n;
+  reg         init   ;
+  reg         next   ;
+  reg [127:0] h0     ;
+  reg [127:0] x      ;
 
 /**********************************************************************
-* output as wires 
+* output as wires
 **********************************************************************/
- wire [127:0] y     ;
- wire         ready ;
+  wire [127:0] y    ;
+  wire         ready;
 
 /**********************************************************************
-* DUT instantiation 
+* DUT instantiation
 **********************************************************************/
-gcm_ghash DUT(
-  .clk    (clk    ),
-  .reset_n(reset_n),
-  .init   (init   ),
-  .next   (next   ),
-  .h0     (h0     ),
-  .x      (x      ),
-  .y      (y      ),
-  .ready  (ready  ) 
-);
+  gcm_ghash DUT (
+    .clk    (clk    ),
+    .reset_n(reset_n),
+    .init   (init   ),
+    .next   (next   ),
+    .h0     (h0     ),
+    .x      (x      ),
+    .y      (y      ),
+    .ready  (ready  )
+  );
 
 /**********************************************************************
 * Define Clock Cycles
 **********************************************************************/
-localparam CLK_PRD=10;
-initial clk='1;
-always #((CLK_PRD)/2) clk = ! clk ; 
+  localparam CLK_PRD = 10 ;
+  initial    clk     = 'b1;
+  always #((CLK_PRD)/2) clk = ! clk ;
 
-task reset_ghash();
-	reset_n =1'b0;
-	@(posedge clk) repeat (5)
-	reset_n = 1'b1;
-endtask
+  task reset_ghash();
+    begin
+      reset_n =1'b0;
+      @(posedge clk) repeat (5)
+        reset_n = 1'b1;
+    end
+  endtask
 
-task run_ghash_test();
-init  
-next  
-h0    
-x     
-endtask 
+  task run_ghash_test();
+    begin
+      h0    = $random();
+      x     = $random();
+    end
+  endtask
 
 /**********************************************************************
 * Stimulus here
 **********************************************************************/
 
-initial
-begin 
-	reset_ghash();
-	run_ghash_test();
-end
+  initial
+    begin
+      reset_ghash();
+      run_ghash_test();
+    end
 
 endmodule
