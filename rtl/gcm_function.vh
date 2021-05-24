be234282c92beda2ac1d8eda149ec39d24205bd3
sohailnagra <31482995+sohailnagra@users.noreply.github.com>
2021-05-25 01:05:17 +0500

    gmul128 implementation

--- rtl/gcm_function.vh
+++ rtl/gcm_function.vh
@@ -2,4 +2,30 @@ function incremnt_gcm (input [127:0] j0);
 	begin 
 		incremnt_gcm = j0 + 1 ; 
 	end
-endfunction : 
+endfunction 
+
+
+function [127:0] gcm_mul_128 (input [127:0] x, input [127:0] y, input [127:0] r);
+		integer      i;
+		reg [127:0] z;
+		reg [127:0] v;
+	begin
+		// 1. copy x into  v and z as 0
+		 z='h0;
+		 v=x;
+
+		for (i = 0; i < 127; i=i+1) begin
+			if (y[i]) begin
+				// do bit wise xor
+				z=z^v;
+			end
+			if (!v[127]) begin
+				// right shift once
+				v=v>>1'b1;
+			end
+			else
+				v= (v>>1'b1) ^ r;
+		end
+		gcm_mul_128 = z; 
+	end
+endfunction  
--- rtl/gcm_ghash.v
+++ rtl/gcm_ghash.v
@@ -19,120 +19,48 @@
 //  on revision history.
 **********************************************************************************/
 
-module gcm_ghash (
-  input  wire         clk    ,
-  input  wire         reset_n,
-  input  wire         init   ,
-  input  wire         next   ,
-  input  wire [127:0] h0     ,
-  input  wire [127:0] x      ,
-  output wire [127:0] y      ,
-  output wire         ready
+module gcm_ghash #(parameter NUM_BLK=1) (
+  input                       clk        ,
+  input                       reset      ,
+  input  wire [127*NUM_BLK:0] m_string   , // that is 128*m
+  input  wire [        127:0] pre_hash   , // preveious rnd_hash
+  input  wire [        127:0] hash_subkey, // preveious rnd_hash
+  input  wire [        127:0] r_mul_plol ,
+  input  wire                 zero_block ,
+  output      [        127:0] ghash
 );
 
+  `include "gcm_function.vh"
 
-  //----------------------------------------------------------------
-  // Gaolis multiplication functions.
-  //----------------------------------------------------------------
-  function [127 : 0] gm2(input [127 : 0] op);
+  reg [127:0] blck_msg ;
+  reg [127:0] ghash_cmb;
+  reg [127:0] ghash_ff ;
+// ghash logic here
+
+  always@(*)
     begin
-      gm2 = {op[126 : 0], 1'b0} ^ (128'h1b & {128{op[127]}});
-    end
-  endfunction // gm2
-
-  function [127 : 0] gm3(input [127 : 0] op);
-    begin
-      gm3 = gm2(op) ^ op;
-    end
-  endfunction // gm3
-
-  function [127 : 0] gm4(input [127 : 0] op);
-    begin
-      gm4 = gm2(gm2(op));
-    end
-  endfunction // gm4
-
-  function [127 : 0] gm7(input [127 : 0] op);
-    begin
-      gm7 = gm4(op) ^ gm3(op);
-    end
-  endfunction // gm7
-
-  function [127 : 0] gm8(input [127 : 0] op);
-    begin
-      gm8 = gm2(gm4(op));
-    end
-  endfunction // gm8
-
-  function [127 : 0] gm16(input [127 : 0] op);
-    begin
-      gm16 = gm4(gm4(op));
-    end
-  endfunction // gm16
-
-  function [127 : 0] gm128(input [127 : 0] op);
-    begin
-      gm128 = gm8(gm16(op));
-    end
-  endfunction // gm128
-
-
-  //----------------------------------------------------------------
-  // Registers including update variables and write enable.
-  //----------------------------------------------------------------
-  reg [127:0] op_reg ;
-  reg [127:0] res_new;
-  reg [127:0] res_reg;
-
-  reg [127:0] pipe1_reg;
-  reg [127:0] pipe1_new;
-  reg [127:0] pipe2_reg;
-  reg [127:0] pipe2_new;
-
-
-  //----------------------------------------------------------------
-  // Concurrent connectivity for ports etc.
-  //----------------------------------------------------------------
-  wire [127:0]  x;
-
-  assign op = x; 
-  assign y  = res_reg;
-
-
-  //----------------------------------------------------------------
-  // reg_update
-  //
-  // Update functionality for all registers in the core.
-  // All registers are positive edge triggered with synchronous
-  // active low reset. All registers have write enable.
-  //----------------------------------------------------------------
-  always @ (posedge clk or negedge reset_n)
-    begin : reg_update
-      if (!reset_n)
-        begin
-          op_reg    <= 128'h0;
-          res_reg   <= 128'h0;
-          pipe1_reg <= 128'h0;
-          pipe2_reg <= 128'h0;
+      if(zero_block) begin
+        blck_msg  = m_string^'h0;
+        ghash_cmb = gcm_mul_128 (blck_msg,hash_subkey,r_mul_plol);
         end
-      else
-        begin
-          op_reg    <= op;
-          res_reg   <= res_new;
-          pipe1_reg <= pipe1_new;
-          pipe2_reg <= pipe2_new;
-        end
-    end // reg_update
+        else
+          begin
+            blck_msg = m_string ^ pre_hash;
+          ghash_cmb = gcm_mul_128 (blck_msg,hash_subkey,r_mul_plol);
+          end
+      end
 
 
-  //----------------------------------------------------------------
-  // gmult_logic
-  //----------------------------------------------------------------
-  always @*
-    begin : gmult_logic
-      pipe1_new = gm128(op_reg);
-      pipe2_new = gm7(op_reg) + gm2(op_reg) + op_reg + 1'b1;
-      res_new   = pipe1_reg + pipe2_reg;
-    end // gmult_logic
+
+    always @(posedge clk or negedge reset)
+      begin
+        if(~reset) begin
+          ghash_ff <= 'h0;
+      end else begin
+        ghash_ff <= ghash_cmb;
+      end
+    end
+
+  assign ghash = ghash_ff;
 
 endmodule // gcm_gmult
